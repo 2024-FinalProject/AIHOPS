aca6320c6f8015b85e34b769bd558148949bb930
shakedmatityahu <matshake@post.bgu.ac.il>
2024-12-29 17:54:14 +0200

    welcome page styling

--- AIHOPS/Front/src/App.jsx
+++ AIHOPS/Front/src/App.jsx
@@ -4,12 +4,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider, useAuth } from "./context/AuthContext";
 
 // Importing the components
-import NavBar from "./Components/NavBar";
+import NavBar from "./components/NavBar";
 
 // Importing the pages
-import WelcomePage from "./Pages/WelcomePage";
-import Register from "./Pages/Register";
-import Login from "./Pages/Login";
+import WelcomePage from "./pages/WelcomePage";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 const AppContent = () => {
   const { isAuthenticated, login } = useAuth();
--- AIHOPS/Front/src/Pages/WelcomePage.jsx
+++ AIHOPS/Front/src/Pages/WelcomePage.jsx
@@ -4,28 +4,41 @@ import { useAuth } from "../context/AuthContext";
 import NavBar from "../components/NavBar";
 import "./WelcomePage.css";
 
+
 const WelcomePage = () => {
   const { user: currentUser } = useAuth();
 
   return (
-    <div>
-      <div className="welcome-container">
-        <header className="welcome-header">
-          <h1 className="welcome-title">AIHOPS</h1>
-          <p className="welcome-subtitle">Create a survey or take one :)</p>
-        </header>
-        <div className="upper-subtitle-container">
-          <span className="lower-subtitle">
-            AIHOPS is a survey platform that allows you to create and take
-            surveys and helps you to make better decisions for your
-            organization.
-          </span>
-        </div>
-       
+
+    <div className="content-container">
+      {/* Large Button and Small Button Container */}
+      <div className="large-button-container">
+        <Link to="/main" className="large-button">
+        <span className="button-title">AIHOPS</span>
+          <br />
+          <span className="button-subtitle">Adoption of Innovation by Healthcare Organization Prerequisites Scale</span>
+        </Link>
+        <Link to="/legal" className="small-button">
+          RIGHTS RESERVED LEGAL STATEMENT
+        </Link>
+      </div>
+
+      {/* Vertical Button Container */}
+      <div className="button-container">
+        <Link to="/login" className="button login">
+          Login
+        </Link>
+        <Link to="/register" className="button register">
+          Register
+        </Link>
+        <Link to="/about" className="button about">
+          ABOUT
+        </Link>
+        <Link to="/accessibility" className="button accessibility">
+          ACCESSIBILITY
+        </Link>
       </div>
     </div>
-
-    
   );
 };
 
