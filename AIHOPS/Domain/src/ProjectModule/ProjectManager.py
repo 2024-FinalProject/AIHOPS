cc1cbdd8b69332a4e9d168b60eea903b322ad146
Amihai Blekherov <amihaibl@post.bgu.ac.il>
2025-01-10 12:20:40 +0200

    Fixed a bug in ProjectManager.py - related to the name & desc update - name would update twice, instead of name and desc update

--- AIHOPS/Domain/src/ProjectModule/ProjectManager.py
+++ AIHOPS/Domain/src/ProjectModule/ProjectManager.py
@@ -107,7 +107,7 @@ class ProjectManager():
     def update_project_name_and_desc(self, pid, actor, name, desc):
         project = self._verify_owner(pid, actor)
         project.update_name(name)
-        project.update_name(desc)
+        project.update_desc(desc)
         return ResponseSuccessMsg(f"{pid}: updated name and desc to {project.name}, {project.desc}")
 
     def get_project_progress_for_owner(self, pid, actor):
