2aaa80635500d35c9762229e36ec85f747227cce
idanlobel <76056806+idanlobel@users.noreply.github.com>
2025-02-06 20:53:58 +0200

    Update config.py

--- AIHOPS/Service/config.py
+++ AIHOPS/Service/config.py
@@ -16,7 +16,6 @@ from DAL.Objects import DBProject
 from DAL.Objects import DBPendingRequests
 
 DATABASE_URL = "sqlite:///mydatabase.db"
-# DATABASE_URL = "postgresql://postgres:wxj2bbhDD!@localhost:5432/aihops_db"
 # PostgreSQL: 'postgresql://user:password@localhost/dbname'
 # MySQL:' mysql+pymysql://user:password@localhost/dbname'
 
